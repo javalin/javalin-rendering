06bf583f8eb91e141e92c6cbe03cf150e4c787ce
Dylan700 <dylanwilliams700@gmail.com>
2023-07-31 14:16:51 +1000

    Add module-info.java file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module io.javalin.rendering {
+	requires io.javalin;
+    requires transitive kotlin.stdlib;
+
+    requires static thymeleaf;
+    requires static com.github.mustachejava;
+    requires static freemarker;
+    requires static gg.jte;
+    requires static io.pebbletemplates;
+
+	exports io.javalin.rendering.template;
+	exports io.javalin.rendering.markdown;
+}
