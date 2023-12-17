a72bf389d5c390964c926fa8de8f7eeaf8e0ef81
Dylan700 <dylanwilliams700@gmail.com>
2023-12-18 09:14:36 +1100

    Rearrange module-info.java

--- src/test/java/io/javalin/module-info.java
+++
@@ -1,8 +0,0 @@
-module test {
-	requires io.javalin.rendering;
-    requires transitive kotlin.stdlib;
-
-    requires gg.jte.runtime;
-    requires org.assertj.core;
-    exports io.javalin to org.assertj.core;
-}
